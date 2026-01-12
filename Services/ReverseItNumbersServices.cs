2c2485b78da8906f6deb0bad5ff02da27d6bf2a0
NotWhoops <whoopsplayz@gmail.com>
2026-01-12 12:12:52 -0800

    first commit

---
+++ Services/ReverseItNumbersServices.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VanhornBMC5.Services
+{
+    public class ReverseItNumbersServices
+    {
+        
+    }
+}
