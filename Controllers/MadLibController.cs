dd6c2f36a7c2a0bf026807686e9835750733dd86
NotWhoops <whoopsplayz@gmail.com>
2026-01-13 15:41:26 -0800

    mad lib functions added

--- Controllers/MadLibController.cs
+++ Controllers/MadLibController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VanhornBMC5.Services;
 
 namespace VanhornBMC5.Controllers
 {
@@ -10,6 +12,19 @@ namespace VanhornBMC5.Controllers
     [Route("api/[controller]")]
     public class MadLibController : ControllerBase
     {
+            private readonly MadLibServices _madLibServices;
+
+            public MadLibController(MadLibServices madLibServices)
+            {
+                _madLibServices = madLibServices;
+            }
+            [HttpGet]
+            [Route("MadLib/{noun}/{verb}/{adjective}/{adverb}/{pronoun}")]
+            public string MadLib(string noun, string verb, string adjective, string pronoun, string adverb)
+            {
+                return _madLibServices.
+                MadLib(noun, verb, adjective, pronoun, adverb);
+            }
         
     }
 }
--- Services/MadLibServices.cs
+++ Services/MadLibServices.cs
@@ -7,6 +7,9 @@ namespace VanhornBMC5.Services
 {
     public class MadLibServices
     {
-        
+        public string MadLib(string noun, string verb, string adjective, string pronoun, string adverb)
+        {
+            return $"Hello {noun} you don't have a {adverb} do you? Well either way can you help me aquire a specific item similar to a {adjective}? Well now tell me about your {verb}. What kind of {pronoun} breed is he?"; 
+        }
     }
 }
