b43f7a078c3eb9ae103f66c5b6c2d8c6ea9da524
NotWhoops <whoopsplayz@gmail.com>
2026-01-14 09:12:00 -0800

    added a few things

--- Services/MadLibServices.cs
+++ Services/MadLibServices.cs
@@ -9,7 +9,7 @@ namespace VanhornBMC5.Services
     {
         public string MadLib(string noun, string verb, string adjective, string pronoun, string adverb)
         {
-            return $"Hello {noun} you don't have a {adverb} do you? Well either way can you help me aquire a specific item similar to a {adjective}? Well now tell me about your {verb}. What kind of {pronoun} breed is he?"; 
+            return $"Hello {noun} you don't have a {adverb} do you? Well either way can you help me acquire a specific item similar to a {adjective}? Well now tell me about your {verb}. What kind of {pronoun} breed is he?"; 
         }
     }
 }
--- obj/Debug/net9.0/Todov4.AssemblyInfo.cs
+++ obj/Debug/net9.0/Todov4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6dc72ac07976dc9c6ea31f3eb3770508e02b0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+198472db031987907d85cf85e66e93a188a4ced0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
